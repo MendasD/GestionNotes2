11dabe164226c7a62ca4bed844d51121bba8e579
MendasD <168176998+MendasD@users.noreply.github.com>
2024-10-11 21:10:48 +0000

    Version en cours du 11/10/2024

--- gestion_notes/connexion/views.py
+++ gestion_notes/connexion/views.py
@@ -21,68 +21,9 @@ def Login(request):
                 request.session['user_pk'] = etudiant.pk
                 request.session['last_activity'] = {'name':'connexion','time':timezone.now().isoformat()}
                 nb_messages_non_lus = Message.objects.filter(etudiant=etudiant).filter(lu=False).count()
-                
-                # On recupere ses notes
-                theme = request.session['theme'] if 'theme' in request.session else ''
-
-                etudiant = Etudiant.objects.get(pk=request.session['user_pk'])
-                notes_etudiant = Note.objects.filter(etudiant=etudiant)
-
-                nb_messages_non_lus = Message.objects.filter(etudiant=etudiant, lu=False).count()
-
-                # Obtenir les annees passees à l'école
-                annees_scolaires = etudiant.years_at_school
-                
-                # Recuperer l'annee selectionner, depuis la methode GET effectuee
-                selected_year = request.GET.get('annee_scolaire', etudiant.annee_scolaire_en_cours)
-                
-                # filtrer les notes de l'etudiant suivant le selected year
-                notes_etudiant_annee = notes_etudiant.filter(annee_scolaire=selected_year)
-
-                class_annee = notes_etudiant_annee.first().classe if notes_etudiant_annee.exists() else None
-                
-                # On recupere toutes les matieres pour la classe correspondante
-                matieres_classe = Matiere.objects.filter(classe=class_annee)
-                
-                # On range les matieres suivant le semestre
-                matieres_semestre1 = matieres_classe.filter(semestre='semestre1')
-                matieres_semestre2 = matieres_classe.filter(semestre='semestre2')
-                
-                # On initialise les dictionnaires pour stocker les notes par matière et semestre
-                notes_par_semestre1 = {}
-                notes_par_semestre2 = {}
-
-                # On remplit les notes pour les matieres du semestre 1
-                for matiere in matieres_semestre1:
-                    note1 = notes_etudiant_annee.filter(matiere=matiere, semestre='semestre1', type_note='note1').first()
-                    note2 = notes_etudiant_annee.filter(matiere=matiere, semestre='semestre1', type_note='note2').first()
-                    notes_par_semestre1[matiere] = [
-                        note1.note if note1 else '/', 
-                        note2.note if note2 else '/'
-                    ]
-
-                #  On remplit les notes pour les matieres du semestre 2
-                for matiere in matieres_semestre2:
-                    note1 = notes_etudiant_annee.filter(matiere=matiere, semestre='semestre2', type_note='note1').first()
-                    note2 = notes_etudiant_annee.filter(matiere=matiere, semestre='semestre2', type_note='note2').first()
-                    notes_par_semestre2[matiere] = [
-                        note1.note if note1 else '/', 
-                        note2.note if note2 else '/'
-                    ]
-                
                 messages.success(request, f'Bienvenue {etudiant.name}')
-
-                return render(request, 'note_etudiant.html', {
-                    'etudiant': etudiant,
-                    'notes_semestre1': notes_par_semestre1,
-                    'notes_semestre2': notes_par_semestre2,
-                    'annees_scolaires': annees_scolaires,
-                    'selected_year': selected_year,
-                    'classe_annee': class_annee,
-                    'theme': theme,
-                    'nb_message': nb_messages_non_lus
-                })
-
+                
+                return redirect('notes_etudiants')
                 
             else:
                 error_message = "Mot de passe incorrect"
